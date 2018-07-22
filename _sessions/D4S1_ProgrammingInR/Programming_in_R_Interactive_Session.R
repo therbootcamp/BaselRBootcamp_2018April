a365db4fedd68e668aa425777868b8ff1a793f01
mdsteiner <markus.steiner@unibas.ch>
2018-07-22 14:31:56 +0200

    updates

---
+++ _sessions/D4S1_ProgrammingInR/Programming_in_R_Interactive_Session.R
@@ -0,0 +1,30 @@
+# RBootcamp - Programming in R - Interactive Session
+
+# sample function (with and without replace)
+
+# paste and paste0 functions
+
+# conditional statements
+
+### logical expressions
+
+# ==, <, > etc
+
+# any and all functions
+
+# chaining of logical expressions with & and |
+
+# %in%
+
+### loops
+
+# for loop
+
+# while loop
+
+
+### functions
+
+# general structure
+
+# warnings and stop
